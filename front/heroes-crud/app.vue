891017e95a29cd521ffc69f192a0b63c948ba0cf
Amospk2 <Googletraduzpramin@hotmail.com>
2023-06-17 18:51:52 +0000

    first commit

---
+++ front/heroes-crud/app.vue
@@ -0,0 +1,20 @@
+
+<script >
+export default {
+  data: () => ({
+    data: '',
+    apiToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySW5mbyI6eyJpZCI6MSwibWV0YWRhdGEiOm51bGwsIm5hbWUiOiJyb290IiwidXNlcm5hbWUiOiJyb290In0sImV4cCI6MTY4NzA0MDEwMywibmJmIjoxNjg3MDE4NTAzfQ.n0bmEVhKb65wTrMS0dI_33h_qN20EzC9JjRTyUYLJOQ"
+
+  }),
+  methods: {
+    async getData() {
+      this.data = await useFetch('http://127.0.0.1:8005/prest/public/heroes?_select=Rank,', { headers: { Authorization: `Bearer ${this.apiToken}` } })
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <button @click="getData">Count is: {{ data }}</button>
+  </div>
+</template>
---
+++ server/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker-compose exec prest prestd migrate up auth
+
+docker-compose exec postgres psql -d prest -U prest -c "INSERT INTO prest_users (name, username, password) VALUES ('root', 'root', MD5('password'))"
+
+docker-compose exec postgres psql -d prest -U prest -c "CREATE TABLE public.heroes (
+    \"Sr. No.\" INTEGER,
+    \"Rank\" VARCHAR(255),
+    \"Class\" VARCHAR(255),
+    \"Name\" VARCHAR(255),
+    \"Gender\" VARCHAR(255),
+    \"Abilities\" VARCHAR(255)
+)"
+
+docker-compose exec postgres psql -d prest -U prest -c "
+COPY heroes(\"Sr. No.\", \"Rank\", \"Class\", \"Name\", \"Gender\", \"Abilities\") FROM '/heroes.csv' DELIMITER ',' CSV HEADER; 
+"
