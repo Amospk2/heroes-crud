6c927c1d3e97e890c05e0c38c52b45cc75e5c244
Wesley Santos Costa <wesleycosta06@hotmail.com>
2023-06-26 10:56:04 -0300

    feat: pagina de create funcional e wip da adição do layout

---
+++ front/heroes-crud/layouts/default.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+  mounted() {
+    this.drawer = false || !this.$vuetify.display.mobile
+  },
+}
+</script>
+
+<template>
+  <v-app>
+    <v-layout>
+      <AppBar :drawer="drawer" @handleDrawer="handleDrawer" />
+      <NavigationDrawer v-model="drawer" />
+
+      <v-main>
+        
+      </v-main>
+    </v-layout>
+  </v-app>
+</template>
--- front/heroes-crud/pages/createHeroes.vue
+++ front/heroes-crud/pages/createHeroes.vue
@@ -1,30 +1,115 @@
 <template>
-  <v-card>
-    <v-layout>
-      <v-app-bar title="Heroes Crud" @click.native="openDrawer"></v-app-bar>
-      
-      <NavigationDrawer />
-      <div style="margin: 8%;">
-        <p>WIP-Create Heroes</p>
-      </div>
+    <v-card>
+        <v-layout>
+            <v-app-bar title="Heroes Crud" @click.native="openDrawer"></v-app-bar>
 
-      <v-main style="min-height: 300px;"></v-main>
-
-    </v-layout>
-  </v-card>
+            <NavigationDrawer />
+            <v-main>
+                <v-container>
+                    <v-row>
+                        <v-col align="center">
+                            <h1 class="text--underline">Create your hero</h1>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="itens.name" label="Name"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="itens.gender" label="Gender"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="itens.rank" label="Rank"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field v-model="itens.class" label="Class"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="10" md="8">
+                            <v-text-field v-model="itens.abilities" label="Abilities"></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue-darken-1" variant="text" @click="close">
+                            Cancel
+                        </v-btn>
+                        <v-btn color="blue-darken-1" variant="text" @click="save">
+                            Save
+                        </v-btn>
+                    </v-card-actions>
+                </v-container>
+            </v-main>
+        </v-layout>
+    </v-card>
 </template>
 
 <script>
 import NavigationDrawer from '~/components/NavigationDrawer.vue'
 
 export default {
-  components: {
-    NavigationDrawer,
-  },
-  methods: {
-    openDrawer() {
-      this.$refs.drawer.drawer = true;
+    data() {
+        return {
+            itens: {
+                name: '',
+                gender: '',
+                rank: '',
+                class: '',
+                abilities: '',
+            },
+            editedIndex: ref(-1),
+            pageCount: ref(0),
+            config: useRuntimeConfig(),
+            items: ref([]),
+        }
     },
-  },
+    components: {
+        NavigationDrawer,
+    },
+    methods: {
+        openDrawer() {
+            this.$refs.drawer.drawer = true
+        },
+        async getPageCount() {
+            const data = await $fetch(`${this.config.public.API_URL}/prest/public/heroes?_count=*`, {
+                headers: { Authorization: `Bearer ${this.config.public.API_TOKEN}` }
+            })
+
+            this.pageCount = data[0].count
+        },
+        async save() {
+            let method = 'POST'
+            let endpoint = ''
+            const editedItem = this.itens
+            let body = {
+                'Name': editedItem.name,
+                'Gender': editedItem.gender,
+                'Rank': editedItem.rank,
+                'Class': editedItem.class,
+                'Abilities': editedItem.abilities
+            }
+            if (this.editedIndex > -1) {
+                method = 'PUT'
+                endpoint = `?HeroID=${editedItem.value["HeroID"]}`
+            } else {
+                body['HeroID'] = this.pageCount + 1
+            }
+            await this.getPageCount()
+            await $fetch(`${this.config.public.API_URL}/prest/public/heroes${endpoint}`, {
+                headers: { Authorization: `Bearer ${this.config.public.API_TOKEN}` },
+                method: method,
+                body: body,
+            }).then(data => {
+                if (!data.error) {
+                    if (this.editedIndex > -1) {
+                        Object.assign(this.items[this.editedIndex], editedItem.value)
+                    } else {
+                        this.items.push(editedItem.value)
+                    }
+                }
+            })
+            close()
+            navigateTo('/home')
+        }
+    }
 }
 </script>
